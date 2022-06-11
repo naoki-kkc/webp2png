92823f99bb6e64076b5c63264c8d5f26c6e83c94
naoki-kkc <naoki2kuchi@gmail.com>
2022-06-12 01:34:07 +0900

    modify code (add target dirs)

--- webp2png.py
+++ webp2png.py
@@ -4,26 +4,24 @@ import os
 
 from PIL import Image
 
-parent_path = "/Users/parent_dir_path"
-
 # parent_path直下の対象フォルダ
-dirs_list = [
-    "dir_1",
-    "dir_2",
-    "dir_3"
+target_dirs_list = [
+    "/Users/parent_dir_pathdir_1",
+    "/Users/parent_dir_pathdir_2",
+    "/Users/parent_dir_pathdir_3"
 ]
 
-for p in dirs_list:
-    target_dir_path = parent_path + p
-    print(target_dir_path)
+for target_dir in target_dirs_list:
 
-    file_list = glob.glob(os.path.join(target_dir_path,"*.webp"))
+    print(target_dir)
+
+    file_list = glob.glob(os.path.join(target_dir,"*.webp"))
 
     cnt = 1
     for f in file_list:
         # print("[" + str(cnt) + "/" + str(len(file_list)) + "]" +f)
         filename     = os.path.split(f)[1]
-        mod_filename = os.path.join(target_dir_path, filename.split(".")[0] + ".png")
+        mod_filename = os.path.join(target_dir, filename.split(".")[0] + ".png")
 
         im = Image.open(f).convert("RGB")
         im.save(mod_filename, "png")
