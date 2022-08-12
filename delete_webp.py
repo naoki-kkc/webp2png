4f27e392194c01c3ef035b64228d5be8c5adb293
naoki-kkc <naoki2kuchi@gmail.com>
2022-08-12 16:26:11 +0900

    add delete_webp.py

---
+++ delete_webp.py
@@ -0,0 +1,57 @@
+import glob
+import os
+from concurrent.futures import ThreadPoolExecutor
+import time
+import math
+import sys
+
+def delete_files_by_regex(thread_cnt, target_dir, target_file_regex):
+
+    start_time = time.time()
+
+    target_files = glob.glob(target_file_regex)
+
+    if len(target_files) == 0:
+        print(f'[{thread_cnt}] skipped (no target).')    
+
+    print('['+thread_cnt+'] ' + target_dir + ' (file_count = ' + str(len(target_files)) + ')')
+
+    for target_file in target_files:
+
+        os.remove(target_file)
+
+    end_time     = time.time()
+    elapsed_time = math.floor(end_time - start_time)
+
+    print(f'[{thread_cnt}] done. file_count:{str(len(target_files))} elapsed_time:{elapsed_time}')    
+
+def main():
+
+    args = sys.argv
+    if len(args) != 2:
+        print('parent_path is not specified.')
+        exit()
+    elif not os.path.exists(args[1]):
+        print('parent_path is not found')
+        exit()
+
+    parent_path = args[1]
+    target_dirs = os.listdir(parent_path)    
+
+    tpe = ThreadPoolExecutor(max_workers=4)
+    thread_cnt = 1
+    for target_dir in target_dirs:
+
+        if target_dir == '.DS_Store':
+            # print('continue by dsstore')
+            continue
+        
+        target_file_regex = os.path.join(parent_path, os.path.join(target_dir, '*.webp'))
+
+        tpe.submit(delete_files_by_regex, str(thread_cnt), target_dir, target_file_regex)
+        thread_cnt += 1
+
+    tpe.shutdown()
+
+if __name__ == '__main__':
+    main()
